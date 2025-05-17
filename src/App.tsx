7d4e4d1abc78cf54743a180830a040f83bf9b927
Peter van der Noord <peterdunord@gmail.com>
2025-05-17 12:45:00 +0200

    Adjsut styling to have white bg in site

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,20 @@ import { AudioContextProvider } from "./context/AudioContextProvider";
 function App() {
   return (
     <AudioContextProvider>
-      <div className="app-container">
-        <h1>Morphagene editor</h1>
-        <div className="content-wrapper">
-          <Routes>
-            <Route path={routes.home} element={<HomePage />} />
-            <Route path={routes.folder} element={<FolderPage />} />
-            <Route path={routes.reel} element={<ReelPage />} />
-          </Routes>
+      <div className="app-layout">
+        <header className="app-header">
+          <div className="header-content">
+            <h1>Morphagene Editor</h1>
+          </div>
+        </header>
+        <div className="app-container">
+          <div className="content-wrapper">
+            <Routes>
+              <Route path={routes.home} element={<HomePage />} />
+              <Route path={routes.folder} element={<FolderPage />} />
+              <Route path={routes.reel} element={<ReelPage />} />
+            </Routes>
+          </div>
         </div>
       </div>
     </AudioContextProvider>
