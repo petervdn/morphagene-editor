dc5fb82031c2257d8b0930164c0b5d0423f0bb19
Peter van der Noord <peterdunord@gmail.com>
2025-05-17 22:06:12 +0200

    Move some logic to useElementSize hook

--- src/components/WaveformView/WaveformView.tsx
+++ src/components/WaveformView/WaveformView.tsx
@@ -1,9 +1,10 @@
 import { useState, useRef, useEffect, type ReactElement } from "react";
 import styles from "./WaveformView.module.css";
 import { drawWaveform } from "../../utils/canvas/drawWaveform";
-import type { Size, ViewPort } from "../../types/types";
+import type { ViewPort } from "../../types/types";
 import type { Splice } from "../../utils/getSplices";
 import { drawSplices } from "../../utils/canvas/drawSplices";
+import { useElementSize } from "../../utils/hooks/useElementSize";
 
 type Props = {
   audioBuffer: AudioBuffer;
@@ -11,8 +12,8 @@ type Props = {
 };
 
 export function WaveformView({ audioBuffer, splices }: Props): ReactElement {
-  const [canvasSize, setCanvasSize] = useState<Size | null>(null);
   const wrapperRef = useRef<HTMLDivElement>(null);
+  const wrapperSize = useElementSize({ elementRef: wrapperRef });
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [viewPort, setViewport] = useState<ViewPort | null>(null);
 
@@ -22,27 +23,6 @@ export function WaveformView({ audioBuffer, splices }: Props): ReactElement {
       to: audioBuffer.duration,
     });
   }, [audioBuffer]);
-
-  useEffect(() => {
-    if (!wrapperRef.current) return;
-
-    const currentWrapper = wrapperRef.current;
-
-    function updateSize() {
-      const { width, height } = currentWrapper.getBoundingClientRect();
-      setCanvasSize({ width, height });
-    }
-
-    updateSize();
-
-    const resizeObserver = new ResizeObserver(updateSize);
-    resizeObserver.observe(currentWrapper);
-
-    return () => {
-      resizeObserver.unobserve(currentWrapper);
-      resizeObserver.disconnect();
-    };
-  }, []);
 
   useEffect(() => {
     if (!canvasRef.current || !viewPort) {
@@ -57,20 +37,20 @@ export function WaveformView({ audioBuffer, splices }: Props): ReactElement {
     });
 
     drawSplices({ context, splices, viewPort });
-  }, [audioBuffer, canvasSize, splices, viewPort]);
+  }, [audioBuffer, splices, viewPort]);
 
   useEffect(() => {}, []);
 
   return (
     <div className={styles.wrapper} ref={wrapperRef}>
-      {canvasSize && (
+      {wrapperSize && (
         <canvas
           ref={canvasRef}
-          width={canvasSize.width * window.devicePixelRatio}
-          height={canvasSize.height * window.devicePixelRatio}
+          width={wrapperSize.width * window.devicePixelRatio}
+          height={wrapperSize.height * window.devicePixelRatio}
           style={{
-            width: `${canvasSize.width}px`,
-            height: `${canvasSize.height}px`,
+            width: `${wrapperSize.width}px`,
+            height: `${wrapperSize.height}px`,
           }}
         />
       )}
---
+++ src/utils/hooks/useElementSize.ts
@@ -0,0 +1,33 @@
+import { useEffect, useState, type RefObject } from "react";
+import type { Size } from "../../types/types";
+
+type Props = {
+  elementRef: RefObject<HTMLElement | null>;
+};
+
+export function useElementSize({ elementRef }: Props) {
+  const [elementSize, setElementSize] = useState<Size | null>(null);
+
+  useEffect(() => {
+    if (!elementRef.current) return;
+
+    const element = elementRef.current;
+
+    function updateSize() {
+      const { width, height } = element.getBoundingClientRect();
+      setElementSize({ width, height });
+    }
+
+    updateSize();
+
+    const resizeObserver = new ResizeObserver(updateSize);
+    resizeObserver.observe(element);
+
+    return () => {
+      resizeObserver.unobserve(element);
+      resizeObserver.disconnect();
+    };
+  }, [elementRef]);
+
+  return elementSize;
+}
